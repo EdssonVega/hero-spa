42567ea9f1624200c8fc65fa222348ad450e789b
Edsson <e_d_sx2@hotmail.com>
2025-03-31 23:24:34 -0400

    fixing messages on heroes search

--- src/components/HeroesResult/HeroesResult.jsx
+++ src/components/HeroesResult/HeroesResult.jsx
@@ -1,24 +1,41 @@
 import React from "react";
+import queryString from "query-string"
+import { useLocation } from "react-router-dom";
 import { HeroCard } from "../HeroCard/HeroCard";
 import "./heroesResultStyles.css"
 
 export const HeroesResult = ({ heroes }) => {
+    const location = useLocation()
+    const query = queryString.parse(location.search).q
+    console.log(query)
+
   return (
     <div className="resultsContainer">
       <h2>Results</h2>
       <hr />
-      {heroes.length > 0 &&(
-        heroes.map((hero) => (
-          <HeroCard
-            key={hero.id}
-            id={hero.id}
-            superhero={hero.superhero}
-            publisher={hero.publisher}
-            alter_ego={hero.alter_ego}
-            first_appearance={hero.first_appearance}
-            characters={hero.characters}
-          />
-        )))}
+      {
+        heroes.length > 0 ? (
+            heroes.map((hero) => (
+                <HeroCard
+                  key={hero.id}
+                  id={hero.id}
+                  superhero={hero.superhero}
+                  publisher={hero.publisher}
+                  alter_ego={hero.alter_ego}
+                  first_appearance={hero.first_appearance}
+                  characters={hero.characters}
+                />
+        ))) : (
+            (heroes.some(hero => hero.superhero.includes(query))) === true ? (
+                <div className="searchAHero">Search a hero</div>
+
+            ): (
+                <div className="noFoundedHero"> No founded hero with {query}</div>
+            )
+            )
+      }
+    
+        
     </div>
   );
 };
