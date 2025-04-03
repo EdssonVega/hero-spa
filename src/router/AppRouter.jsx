92b82745dd99fae693ee4be090c1df08a1df2857
Edsson <e_d_sx2@hotmail.com>
2025-04-03 00:17:30 -0400

    added public routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,16 +3,24 @@ import { Routes, Route, Navigate} from 'react-router';
 import { Login } from '../pages/Login';
 import { HeroesRoutes } from './HeroesRoutes';
 import { PrivateRouter } from './PrivateRouter';
+import { PublicRoute } from './PublicRoute';
 
 const AppRouter = () => {
   return (
         <Routes >
-            <Route path='login' element={<Login/>}/>
+
+          <Route path='/login' element={
+            <PublicRoute>
+              <Login/>
+            </PublicRoute>
+          } />
+
             <Route path='/*' element={
               <PrivateRouter>
                 <HeroesRoutes />
               </PrivateRouter>
             } />
+  {/*<Route path='login' element={<Login/>}/>*/} 
         </Routes>        
   )
 }
