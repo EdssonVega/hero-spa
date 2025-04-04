5b75b40c8ce11e1712836fb79ded14f13912d150
Edsson <e_d_sx2@hotmail.com>
2025-04-04 12:48:36 -0400

    added styles to login and installed react icons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,32 @@
-import React, { useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { AuthContext } from '../auth/context/AuthContext'
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../auth/context/AuthContext";
+import "./pagesStyles.css";
 
 export const Login = () => {
-    const{login} = useContext(AuthContext)
-    const navigate = useNavigate()
-    const onLogIn = () => {
-        login("Edsson Vega");
-        
-        navigate("/",{
-            replace:true
-        })
-    }
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const onLogIn = () => {
+    login("Edsson Vega");
+
+    navigate("/", {
+      replace: true,
+    });
+  };
   return (
-    <div>
-        <h1>Log in with your account!</h1>
-        <button onClick={onLogIn}>
-            Log in
-        </button>
+    <div className="loginPageContainer">
+        <div>
+        <h1>Welcome <br /> Back!</h1>
+        <p>Be prepare to see the mighty heros of the universe!</p>
+        <p>and don't forget to contact me:</p>
+        </div>
+      <div className="loginForm">
+        <label htmlFor="">User:</label>
+        <input type="text" />
+        <label htmlFor="">Password:</label>
+        <input type="password" />
+        <button onClick={onLogIn}>Log in</button>
+      </div>
     </div>
-  )
-}
+  );
+};
