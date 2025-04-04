9ba7bc56e5262b9a47c30e2167384f4b66cad1d9
Edsson <e_d_sx2@hotmail.com>
2025-04-04 14:11:50 -0400

    finished errors in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,52 @@
-import React, { useContext } from "react";
+import React, { useContext,useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../auth/context/AuthContext";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
+import { BsExclamationCircle } from "react-icons/bs";
 import "./pagesStyles.css";
 
 export const Login = () => {
+    const [user, setUser] = useState("Edsson Vega")
+    const [password, setPassword] = useState("genericPassword123")
+    const [errors, setErrors] = useState({
+        user:"",
+        password:""
+    })
+
+    const onChangeUser = ( e) => {
+        setUser(e.target.value)
+        console.log(user)
+    }
+
+    const onChangePassword = ( e) => {
+        setPassword(e.target.value)
+    }
+
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
   const onLogIn = () => {
-    login("Edsson Vega");
+    if(user !=="Edsson Vega" || password !=="genericPassword123"){
+        if(user !== "Edsson Vega"){
+            setErrors((prevState) => ({
+                    ...prevState, user: "Please use the default user Edsson Vega to sign in"
+                })
+            )
+        } 
+        if(password !=="genericPassword123"){
+            setErrors((prevState) => ({
+                    ...prevState, password: "Please use the default password genericPassword123 to sign in"
+                })
+            )
+        } 
+        return}
+    login(user);
 
     navigate("/", {
       replace: true,
     });
   };
+
   return (
     <div className="loginPageContainer">
         <div>
@@ -30,14 +62,20 @@ export const Login = () => {
         </div>
       <div className="loginForm">
         <h2>Sign in</h2>
-        <label htmlFor="">User:</label>
-        <input type="text" />
-        <label htmlFor="">Password:</label>
-        <input type="password" />
+        <label htmlFor="user">User:</label>
+        <input id="user" onChange={onChangeUser} type="text" value={user}/>
+        {errors.user !== "" && (
+            <p className="messageError"> <BsExclamationCircle />{errors.user}</p>
+        )}
+        <label htmlFor="password">Password:</label>
+        <input id="password" onChange={onChangePassword} type="text" value={password}/>
+        {errors.password !== "" && (
+            <p className="messageError"> <BsExclamationCircle />{errors.password}</p>
+        )}
         <button onClick={onLogIn}>
             Sign in now
         </button>
-        <p>Just press on the log in button with those credentials</p>
+        <p className="bewareCredentials">Just press on the sign in button with those credentials</p>
       </div>
     </div>
   );
